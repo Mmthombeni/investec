00d8cea763bd574dc825c5665191abfe9118e751
Malebo MTHOMBENI <mmthombe@e4r9p10.wethinkcode.co.za>
2019-02-07 15:11:35 +0200

    modified Address class and modified HighestCommonFactor

--- Address.java
+++ Address.java
@@ -2,6 +2,12 @@
  * Address
  */
 public class Address {
+    private String _addressType;
+    private String _addressLine;
+    private String _province;
+    private String _city;
+    private String _country;
+    private long   _postalCode;
 
     public Address() {
         
--- HighestCommonFactor.java
+++ HighestCommonFactor.java
@@ -5,14 +5,16 @@
 public class HighestCommonFactor {
 
     public int findHCF(int x, int y) {  //Euclid's methods
+        
         if(x == 0)   //If x is zero then greatest common divisor of both will be y
             return y;
 
-        return findHCF(y % x, x);   //
+        return findHCF(y % x, x);   //if not 0 assign x to y and x becomes the remainder of y/x and check if it's 0 
     }
     
     public int highestCommonFactor(int[] numbers) {
         int result = numbers[0]; //stores the first value
+        
         for(int i = 1; i < numbers.length; i++) 
             result = new HighestCommonFactor().findHCF(numbers[i], result);
 
@@ -21,6 +23,7 @@ public class HighestCommonFactor {
 
     public static void main(String[] args) {
         //should make this array come from a list
+        //handle empty array
         int numArray[] = {2, 4, 6, 8, 16};
         System.out.println(new HighestCommonFactor().highestCommonFactor(numArray)); //calls and prints the highest common factor
     }
