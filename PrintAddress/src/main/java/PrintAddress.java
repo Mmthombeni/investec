70c027b0f3c6df060efbe52177f803422d7f0628
Malebo MTHOMBENI <mmthombe@e4r9p10.wethinkcode.co.za>
2019-02-08 13:21:06 +0200

    reading the json file

--- PrintAddress/src/main/java/PrintAddress.java
+++ PrintAddress/src/main/java/PrintAddress.java
@@ -1,8 +1,11 @@
 
+import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 
 import java.io.FileReader;
+import java.util.Iterator;
+import java.util.Map;
 
 public class PrintAddress {
 
@@ -12,12 +15,38 @@ public class PrintAddress {
 
     public static void main(String[] args) throws Exception {
         try {
+            System.out.println("life" +
+                    "");
+            //Parsing file "addresses.json"
             Object obj = new JSONParser().parse(new FileReader("addresses.json"));
 
-            JSONObject jObj = (JSONObject) obj;
+            //typecasting obj to JSONArray
+            JSONArray jArrary = (JSONArray) obj;
+
+            System.out.println(jArrary.get(0));
+
+            Iterator itr2 = jArrary.iterator();
+
+            while (itr2.hasNext()) {
+               Iterator<Map.Entry> itr1 = ((Map) itr2.next()).entrySet().iterator();
+               while (itr1.hasNext()) {
+                   Map.Entry pair = itr1.next();
+                   //System.out.println(pair.getKey() + " : " + pair.getValue());
+               }
+            }
+
+            //getting some infor to see if it works
+//            Map type = ((Map)jObj.get("type"));
+//
+//            //interate
+//            Iterator<Map.Entry> itr1 = type.entrySet().iterator();
+//            while (itr1.hasNext()) {
+//                Map.Entry pair = itr1.next();
+//                System.out.println(pair.getKey() + " : " + pair.getValue());
+//            }
             
         } catch (Exception e) {
-
+            System.out.println(e);
         }
     }
 }
