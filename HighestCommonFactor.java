1e7d01ab033a01bbe4cb02fd7f2c48fda3c269fd
Malebo MTHOMBENI <mmthombe@e4r9p10.wethinkcode.co.za>
2019-02-11 11:15:05 +0200

    modified comments using the method just learned

--- HighestCommonFactor.java
+++ HighestCommonFactor.java
@@ -3,17 +3,26 @@
  */
 
 public class HighestCommonFactor {
-
-    public int findHCF(int x, int y) {  //Euclid's methods
+    /**
+     * Euclid's methods - if x is zero then greatest common divisor of both x and y will be y, if not assign
+     * x to y and x becomes the remainder(mod) of y/x and check if it's 0
+     * @param x,y the two numbers to compare for highest common factor
+     * @return the HCF of two numbers
+     */
+    public int findHCF(int x, int y) { 
         
-        if(x == 0)   //If x is zero then greatest common divisor of both will be y
+        if(x == 0)
             return y;
 
-        return findHCF(y % x, x);   //if not 0 assign x to y and x becomes the remainder of y/x and check if it's 0 
+        return findHCF(y % x, x); 
     }
-    
+
+    /**
+     * @param numbers the array of numbers that we finding the HCF
+     * @return highest common factor for all the numbers in the array
+     */
     public int highestCommonFactor(int[] numbers) {
-        int result = numbers[0]; //stores the first value
+        int result = numbers[0];
         
         for(int i = 1; i < numbers.length; i++) 
             result = new HighestCommonFactor().findHCF(numbers[i], result);
@@ -21,10 +30,12 @@ public class HighestCommonFactor {
         return result;
     }
 
-    public static void main(String[] args) {
-        //should make this array come from a list
-        //handle empty array
-        int numArray[] = {2, 4, 6, 8, 16};
-        System.out.println(new HighestCommonFactor().highestCommonFactor(numArray)); //calls and prints the highest common factor
+    public static void main(String[] args) throws Exception {
+         try {
+            int numArray[] = {50, 15, 5, 25, 85};
+            System.out.println(new HighestCommonFactor().highestCommonFactor(numArray)); 
+        } catch (Exception e) {
+            System.out.println("The array can't be empty");
+        }
     }
 }
